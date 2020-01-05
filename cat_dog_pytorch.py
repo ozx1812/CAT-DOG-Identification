73d05baa1213234d78a69010b340ce5e8b3e95db
omiz_1812 <39741739+ozx1812@users.noreply.github.com>
2020-01-05 11:17:08 +0530

    initializing the pytorch code file

---
+++ cat_dog_pytorch.py
@@ -0,0 +1,11 @@
+"""
+Transfer Learning
+we will use pre-trained networks to solved challenging problems in computer vision.
+here we will use Neural Net trained on ImageNet dataset .
+
+ImageNet is a massive dataset with over 1 million labeled images in 1000 categories.
+It's used to train deep neural networks using an architecture called convolutional layers. 
+
+Using a pre-trained network on images not in the training set is called transfer learning. 
+Here we'll use transfer learning to train a network that can classify our cat and dog photos with near perfect accuracy.
+"""
