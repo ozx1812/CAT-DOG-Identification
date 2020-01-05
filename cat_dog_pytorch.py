df66960e2ef68afea6f1ad529fbd0db40be9de28
omiz_1812 <39741739+ozx1812@users.noreply.github.com>
2020-01-05 13:31:26 +0530

    Transfer Learning
    using DenseNet trained previously on ImageNet Dataset.

--- cat_dog_pytorch.py
+++ cat_dog_pytorch.py
@@ -228,8 +228,169 @@ class Classifier_cat_dog(nn.Module):
         return x
         
 --------------------------------------------------------------------------------------------------------
+                                            Transfer Learning
+--------------------------------------------------------------------------------------------------------
+"""
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+"""
+--------------------------------------------------------------------------------------------------------
+Most of the pretrained models require the input to be 224x224 images. 
+Also, we'll need to match the normalization used when the models were trained. Each color channel was normalized separately, 
+the means are [0.485, 0.456, 0.406] and the standard deviations are [0.229, 0.224, 0.225].
+--------------------------------------------------------------------------------------------------------
+
+"""
+data_dir = 'Cat_Dog_data'
+
+# TODO: Define transforms for the training data and testing data
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])])
+
+# Pass transforms in here, then run the next cell to see how the transforms look
+train_data = datasets.ImageFolder(data_dir + '/train', transform=train_transforms)
+test_data = datasets.ImageFolder(data_dir + '/test', transform=test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
+
+"""
+--------------------------------------------------------------------------------------------------------
+
+We can load in a model such as DenseNet.(link: http://pytorch.org/docs/0.3.0/torchvision/models.html#id5)
+Let's print out the model architecture so we can see what's going on.
+--------------------------------------------------------------------------------------------------------
 
 """
 
+model = models.densenet121(pretrained=True)
+print(model)
 
+"""
+--------------------------------------------------------------------------------------------------------
+This model is built out of two main parts, the features and the classifier. 
+The features part is a stack of convolutional layers and overall works as a feature detector that can be fed into a classifier.
+The classifier part is a single fully-connected layer (classifier): Linear(in_features=1024, out_features=1000). 
+This layer was trained on the ImageNet dataset, so it won't work for our specific problem. 
+That means we need to replace the classifier, but the features will work perfectly on their own. In general, 
+I think about pre-trained networks as amazingly good feature detectors that can be used as the input for 
+simple feed-forward classifiers.
+--------------------------------------------------------------------------------------------------------
+"""
 
+# Freeze parameters so we don't backprop through them
+for param in model.parameters():
+    param.requires_grad = False
+
+from collections import OrderedDict
+classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(1024, 500)),
+                          ('relu', nn.ReLU()),
+                          ('fc2', nn.Linear(500, 2)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+model.classifier = classifier
+
+"""
+--------------------------------------------------------------------------------------------------------
+With our model built, we need to train the classifier. However, now we're using a really deep neural network. 
+If you try to train this on a CPU like normal, it will take a long, long time. 
+Instead, we're going to use the GPU to do the calculations. 
+The linear algebra computations are done in parallel on the GPU leading to 100x increased training speeds. 
+It's also possible to train on multiple GPUs, further decreasing training time.
+PyTorch, along with pretty much every other deep learning framework, 
+uses CUDA to efficiently compute the forward and backwards passes on the GPU. 
+In PyTorch, you move your model parameters and other tensors to the GPU memory using model.to('cuda'). 
+You can move them back from the GPU with model.to('cpu') which you'll commonly do when you need to operate
+on the network output outside of PyTorch.
+--------------------------------------------------------------------------------------------------------
+"""
+# Use GPU if it's available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = models.densenet121(pretrained=True)
+
+# Freeze parameters so we don't backprop through them
+for param in model.parameters():
+    param.requires_grad = False
+    
+model.classifier = nn.Sequential(nn.Linear(1024, 256),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(256, 2),
+                                 nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+
+model.to(device);
+
+"""
+--------------------------------------------------------------------------------------------------------
+"""
+
+epochs = 1
+steps = 0
+running_loss = 0
+print_every = 5
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        # Move input and label tensors to the default device
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in testloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    
+                    test_loss += batch_loss.item()
+                    
+                    # Calculate accuracy
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    
+            print(f"Epoch {epoch+1}/{epochs}.. "
+                  f"Train loss: {running_loss/print_every:.3f}.. "
+                  f"Test loss: {test_loss/len(testloader):.3f}.. "
+                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+            running_loss = 0
+            model.train()
